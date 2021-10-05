39bf99b738000ae60be75cb56c4ca675554b36d8
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-10-05 09:55:39 -0700

    All working
    Works as desired

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,38 @@
 console.log("octopus cather");
 
+let octo = document.querySelector("#octo");
 let score = 0;
 let timeElapsed = 0;
-// let octo = document.querySelector("#octo");
 let timer = window.setInterval(moveOcto, 1000);
+let secondsGone = document.querySelector("#secondsGone");
+let endMessage = document.querySelector("#endMessage");
+let scoreText = document.querySelector("#scoreText"); 
 
 
+scoreText.innerText = "0";
 
-let octo = document.querySelector("#octo");
+
 
 function moveOcto() {
-    // timeElapsed = timeElapsed + 1; 
+    timeElapsed = timeElapsed + 1; 
     octo.style.left = Math.random()*1000+"px";
     octo.style.top = Math.random()*500 + "px";
+    secondsGone.innerText = timeElapsed;
 
+    if (timeElapsed == 30){
+        clearInterval(timer); 
+        endMessage.innerText = "Time Up! You Socred: " + score;               
+    }
+}
+
+function caughtOcto() {    
+    score++ ;
+    scoreText.innerText =  score; 
 }
 
 
 
-// octo.addEventListener("click", moveOcto);
+octo.addEventListener("mousedown", caughtOcto);
 
 
 
