9a2d4f79631775e9d0c1cf372e54218d259a1701
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-10-18 21:07:26 -0700

    style
    background image adjusted for big screens

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,13 +6,15 @@ let secondsGone = document.querySelector("#secondsGone");
 let endMessage = document.querySelector("#endMessage");
 let scoreText = document.querySelector("#scoreText"); 
 let disctractOcto = document.querySelector("#distract");
+let octoLast = document.querySelector("#last"); 
+
 
 
 let score = 0;
 let timeElapsed = 0;
 disctractOcto.style.visibility='hidden'; 
 
-
+octoLast.style.visibility   = "hidden"; 
 
 
 scoreText.innerText = "0";
@@ -24,18 +26,34 @@ function caughtOcto() {
     console.log(score); 
 }
 
+
+
 function moveOcto() {
     timeElapsed = timeElapsed + 1; 
-    octo.style.left = Math.ceil(Math.random()*1000)+"px";
-    octo.style.top = Math.ceil(Math.random()*800) + "px";
+    octo.style.left = Math.ceil(Math.random()*900)+"px";
+    octo.style.top = Math.ceil(Math.random()*500) + "px";
     secondsGone.innerText = timeElapsed;
 
     if (timeElapsed >15 && timeElapsed <18 ) {
-        disctractOcto.style.visibility = "visible";    
+        disctractOcto.style.visibility = "visible";   
+         
     } else if (timeElapsed == 30){
         clearInterval(timer); 
+        octo.style.visibility= "hidden" ;
         endMessage.innerText = "Time Up! You Socred: " + score; 
         disctractOcto.style.visibility = "visible";
+        // disctractOcto.style.float = "center";
+        octoLast.style.visibility = "visible"; 
+        octoLast.style.left = 750+"px";
+        octoLast.style.top = 400+"px"; 
+        disctractOcto.style.marginRight = "550px"; 
+
+
+
+        
+        
+        // break;
+
     }   
     else {
         disctractOcto.style.visibility = "hidden";
@@ -44,14 +62,6 @@ function moveOcto() {
     
 }
 
-
-
-
-
 octo.addEventListener("mousedown", caughtOcto);
 
 
-
-// } else if (timeElapsed >22 || timeElapsed < 25) {
-        // disctractOcto.style.visibility = 'visible';
-        // disctractOcto.style.float = "top";
