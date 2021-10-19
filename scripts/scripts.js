acede1981f23e178c93589c4fd7e927db60662f3
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-10-18 21:34:23 -0700

    finishing touches given
    alert added, extra code and comments removed. Some extra images deleted too.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,67 +1,56 @@
-console.log("octopus cather");
+// console.log("octopus cather");
+alert("Considering that our octopus has to run around a lot during this game, use of desktop, laptops or bigger screens are recommended.")
 
 let octo = document.querySelector("#octo");
 let timer = window.setInterval(moveOcto, 1000);
 let secondsGone = document.querySelector("#secondsGone");
 let endMessage = document.querySelector("#endMessage");
-let scoreText = document.querySelector("#scoreText"); 
+let scoreText = document.querySelector("#scoreText");
 let disctractOcto = document.querySelector("#distract");
-let octoLast = document.querySelector("#last"); 
+let octoLast = document.querySelector("#last");
 
 
 
 let score = 0;
 let timeElapsed = 0;
-disctractOcto.style.visibility='hidden'; 
+disctractOcto.style.visibility = 'hidden';
 
-octoLast.style.visibility   = "hidden"; 
+octoLast.style.visibility = "hidden";
 
 
 scoreText.innerText = "0";
-secondsGone.innerText = "0"; 
+secondsGone.innerText = "0";
 
-function caughtOcto() {    
-    score = score + 1 ;
-    scoreText.innerText =  score; 
-    console.log(score); 
+function caughtOcto() {
+    score = score + 1;
+    scoreText.innerText = score;
+    // console.log(score);
 }
 
 
 
 function moveOcto() {
-    timeElapsed = timeElapsed + 1; 
-    octo.style.left = Math.ceil(Math.random()*900)+"px";
-    octo.style.top = Math.ceil(Math.random()*500) + "px";
+    timeElapsed = timeElapsed + 1;
+    octo.style.left = Math.ceil(Math.random() * 900) + "px";
+    octo.style.top = Math.ceil(Math.random() * 500) + "px";
     secondsGone.innerText = timeElapsed;
 
-    if (timeElapsed >15 && timeElapsed <18 ) {
-        disctractOcto.style.visibility = "visible";   
-         
-    } else if (timeElapsed == 30){
-        clearInterval(timer); 
-        octo.style.visibility= "hidden" ;
-        endMessage.innerText = "Time Up! You Socred: " + score; 
+    if (timeElapsed > 15 && timeElapsed < 18) {
         disctractOcto.style.visibility = "visible";
-        // disctractOcto.style.float = "center";
-        octoLast.style.visibility = "visible"; 
-        octoLast.style.left = 750+"px";
-        octoLast.style.top = 400+"px"; 
-        disctractOcto.style.marginRight = "550px"; 
 
+    } else if (timeElapsed == 30) {
+        clearInterval(timer);
+        octo.style.visibility = "hidden";
+        endMessage.innerText = "Time Up! You Socred: " + score;
+        disctractOcto.style.visibility = "visible";
 
-
-        
-        
-        // break;
-
-    }   
-    else {
+        octoLast.style.visibility = "visible";
+        octoLast.style.left = 750 + "px";
+        octoLast.style.top = 400 + "px";
+        disctractOcto.style.marginRight = "550px";
+    } else {
         disctractOcto.style.visibility = "hidden";
     }
-
-    
 }
 
 octo.addEventListener("mousedown", caughtOcto);
-
-
